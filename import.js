0741887efa9addf682ee347e502f338cdaa218c0
dygapp <dygapp@qq.com>
2017-12-15 11:48:27 +0800

    1

--- import.js
+++ import.js
@@ -35,7 +35,7 @@ const fields = [
 ];
 
 async function doWork() {
-  console.log('正在读取数据...')
+  console.log('正在读取数据...');
   const lines = await new Promise((resolve, reject) => {
     const res = [];
     const rl = readline.createInterface({
@@ -44,23 +44,23 @@ async function doWork() {
     });
 
     rl.on('line', async line => {
-      //console.log(line);
+      // console.log(line);
       res.push(line);
     });
     rl.on('close', () => {
       resolve(res);
     });
-    rl.on('error', (err) => {
+    rl.on('error', err => {
       reject(err);
     });
   });
-  console.log(`共读取数据${rows.length}条!`);
-  console.log(`正在保存数据...`);
-  let client;
+  console.log(`共读取数据${lines.length}条!`);
+  console.log('正在保存数据...');
   // Use connect method to connect to the Server
-  client = await MongoClient.connect(url, { poolSize: 10 });
+  const client = await MongoClient.connect(url, { poolSize: 10 });
   const db = client.db(dbName).collection('original');
   for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
     const cols = line.split(',');
     const data = fields.reduce(
       (p, c, i) => ({
@@ -69,9 +69,9 @@ async function doWork() {
       }), {});
 
     try {
-      const entity = await db..findOne({ sfzh: data.sfzh });
+      const entity = await db.findOne({ sfzh: data.sfzh });
       if (!entity) {
-        await db..insertOne(data);
+        await db.insertOne(data);
       }
     } catch (err) {
       console.log(`处理错误： ${line}`);
@@ -83,4 +83,4 @@ async function doWork() {
   }
 }
 
-await doWork();
+doWork();
