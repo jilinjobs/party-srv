f672f472d6fbf03a612b245cbc061d3d1297d68d
dygapp <dygapp@qq.com>
2017-12-15 11:43:59 +0800

    import

--- import.js
+++ import.js
@@ -1,3 +1,5 @@
+import { resolve } from 'url';
+
 'use strict';
 
 const readline = require('readline');
@@ -34,34 +36,53 @@ const fields = [
   'wclx',
 ];
 
-const rl = readline.createInterface({
-  input: fs.createReadStream('data.csv').pipe(iconv.decodeStream('GBK')),
-  crlfDelay: Infinity,
-});
+async function doWork() {
+  console.log('正在读取数据...')
+  const lines = await new Promise((resolve, reject) => {
+    const res = [];
+    const rl = readline.createInterface({
+      input: fs.createReadStream('data.csv').pipe(iconv.decodeStream('GBK')),
+      crlfDelay: Infinity,
+    });
 
-rl.on('line', async line => {
-  console.log(line);
-  const cols = line.split(',');
-  const data = fields.reduce(
-    (p, c, i) => ({
-      ...p,
-      [c]: cols[i],
-    }), { });
-
+    rl.on('line', async line => {
+      //console.log(line);
+      res.push(line);
+    });
+    rl.on('close', () => {
+      resolve(res);
+    });
+    rl.on('error', (err) => {
+      reject(err);
+    });
+  });
+  console.log(`共读取数据${rows.length}条!`);
+  console.log(`正在保存数据...`);
   let client;
-  try {
-    // Use connect method to connect to the Server
-    client = await MongoClient.connect(url, { poolSize: 10 });
+  // Use connect method to connect to the Server
+  client = await MongoClient.connect(url, { poolSize: 10 });
+  const db = client.db(dbName).collection('original');
+  for (let i = 0; i < lines.length; i++) {
+    const cols = line.split(',');
+    const data = fields.reduce(
+      (p, c, i) => ({
+        ...p,
+        [c]: cols[i],
+      }), {});
 
-    const db = client.db(dbName);
-    const entity = await db.collection('original').findOne({ sfzh: data.sfzh });
-    if (!entity) {
-      await db.collection('original').insertOne(data);
+    try {
+      const entity = await db..findOne({ sfzh: data.sfzh });
+      if (!entity) {
+        await db..insertOne(data);
+      }
+    } catch (err) {
+      console.log(`处理错误： ${line}`);
+      console.error(err);
     }
-  } catch (err) {
-    console.log(err.stack);
   }
   if (client) {
     client.close();
   }
-});
+}
+
+await doWork();
